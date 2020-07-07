e9658a2586f88d816c901abd39c4ea59b9e3a61b
thowell <thowell@stanford.edu>
2020-07-06 21:53:42 -0700

    sample LQR with pendulum dynamics

--- sampleLQR_pendulum.jl
+++ sampleLQR_pendulum.jl
@@ -62,9 +62,7 @@ function obj(z)
     for t = 1:T-1
         x = z[x_idx[t]]
         u = z[u_idx[t]]
-        # s += (x-xT)'*Q[t]*(x-xT) + u'*R[t]*u
         s += x'*Q[t]*x + u'*R[t]*u
-
     end
     x = z[x_idx[T]]
     s += (x-xT)'*Q[T]*(x-xT)
@@ -80,7 +78,7 @@ function con!(c,z)
         x = z[x_idx[t]]
         u = z[u_idx[t]]
         x⁺ = z[x_idx[t+1]]
-        c[(t-1)*n .+ (1:n)] = x⁺ - dyn_d(x,u,Δt)
+        c[(t-1)*n .+ (1:n)] = x⁺ - dynamics(x,u,Δt)
     end
     c[(T-1)*n .+ (1:n)] = z[x_idx[1]] - x1
     c[T*n .+ (1:n)] = z[x_idx[T]] - xT
@@ -111,36 +109,37 @@ B = []
 for t = 1:T-1
     x = x_sol[t]
     u = u_sol[t]
-    fx(z) = dyn_d(z,u,Δt)
-    fu(z) = dyn_d(x,z,Δt)
+    fx(z) = dynamics(z,u,Δt)
+    fu(z) = dynamics(x,z,Δt)
 
     push!(A,ForwardDiff.jacobian(fx,x))
     push!(B,ForwardDiff.jacobian(fu,u))
 end
 
 Q = [t < T ? Diagonal([10.0;1.0]) : Diagonal([100.0;100.0]) for t = 1:T]
-R = [Matrix((0.001)*I,m,m) for t = 1:T-1]
+R = [Diagonal(ones(m)) for t = 1:T-1]
 
 P = [zeros(n,n) for t = 1:T]
 K = [zeros(m,n) for t = 1:T-1]
+
 P[T] = Q[T]
 for t = T-1:-1:1
     K[t] = (R[t] + B[t]'*P[t+1]*B[t])\(B[t]'*P[t+1]*A[t])
     P[t] = Q[t] + K[t]'*R[t]*K[t] + (A[t]-B[t]*K[t])'*P[t+1]*(A[t]-B[t]*K[t])
 end
 
-# number of samples
-x11 = [1.0; 0.0]
-x12 = [-1.0; 0.0]
-x13 = [0.0; 1.0]
-x14 = [0.0; -1.0]
+β = 100.0
+x11 = β*[1.0; 0.0]
+x12 = β*[-1.0; 0.0]
+x13 = β*[0.0; 1.0]
+x14 = β*[0.0; -1.0]
 
 x1 = [x11,x12,x13,x14]
 
 N = length(x1)
 
 n_nlp = N*(n*(T-1) + m*(T-1)) + m*n*(T-1)
-m_nlp = N*(n*(T-1)) + N*(m*(T-1)) #+ N*(n*(T-1))
+m_nlp = N*(n*(T-1) + m*(T-1))
 
 idx_k = [(t-1)*(m*n) .+ (1:m*n) for t = 1:T-1]
 idx_x = [[(T-1)*(m*n) + (i-1)*(n*(T-1) + m*(T-1)) + (t-1)*(n+m) .+ (1:n) for t = 1:T-1] for i = 1:N]
@@ -148,7 +147,6 @@ idx_u = [[(T-1)*(m*n) + (i-1)*(n*(T-1) + m*(T-1)) + (t-1)*(n+m) + n .+ (1:m) for
 
 idx_con_dyn = [[(i-1)*(n*(T-1)) + (t-1)*n .+ (1:n) for t = 1:T-1] for i = 1:N]
 idx_con_ctrl = [[(i-1)*(m*(T-1)) + N*(n*(T-1)) + (t-1)*m .+ (1:m) for t = 1:T-1] for i = 1:N]
-# idx_con_k = [[N*(m*(T-1)) + N*(n*(T-1)) + (i-1)*(n*(T-1)) + (t-1)*n .+ (1:n) for t = 1:T-1] for i = 1:N]
 
 function obj(z)
     s = 0
@@ -156,7 +154,7 @@ function obj(z)
         for i = 1:N
             x = view(z,idx_x[i][t])
             u = view(z,idx_u[i][t])
-            s += x'*Q[t]*x + u'*R[t]*u
+            s += x'*Q[t+1]*x + u'*R[t]*u
         end
     end
     return s
@@ -178,48 +176,13 @@ end
 
 c0 = rand(m_nlp)
 con!(c0,ones(n_nlp))
-
-# idx_ineq = vcat(vcat(idx_con_k...)...)
-prob = Problem(n_nlp,m_nlp,obj,con!,true)#,idx_ineq=idx_ineq)
+prob = Problem(n_nlp,m_nlp,obj,con!,true)
 
 z0 = rand(n_nlp)
 z_sol = solve(z0,prob)
 
 K_sample = [reshape(z_sol[idx_k[t]],m,n) for t = 1:T-1]
-println("solution error: $([norm(vec(K_sample[t]-K[t])) for t = 1:T-1])")
-z_sol_K = copy(z_sol)
-for t = 1:T-1
-    z_sol_K[idx_k[t]] = vec(K[t])
-    for i = 1:N
-        z_sol_K[idx_u[i][t]] = -K[t]*(t==1 ? x1[i] : z_sol_K[idx_x[i][t-1]])
-        z_sol_K[idx_x[i][t]] = A[t]*(t==1 ? x1[i] : z_sol_K[idx_x[i][t-1]]) + B[t]*z_sol_K[idx_u[i][t]]
-    end
-end
-norm(vec(z_sol - z_sol_K))
-obj(z_sol)
-obj(z_sol_K)
+K_error = [norm(vec(K_sample[t]-K[t]))/norm(vec(K[t])) for t = 1:T-1]
+println("solution error: $(sum(K_error)/N)")
 
-# simulate controller
-T_sim = 10*T
-μ = zeros(2)
-Σ = Diagonal(1.0e-12*rand(2))
-W = Distributions.MvNormal(μ,Σ)
-w = rand(W,T_sim)
-z0_sim = [0.0;0.0]
-
-plt = plot(θ_sol,dθ_sol,xlabel="θ",ylabel="dθ",width=2.0,color=:red)
-z_tvlqr, u_tvlqr = simulate_linear_controller(K,x_sol,u_sol,T_sim,Δt,z0_sim,w)
-x_tvlqr = [z_tvlqr[t][1] for t = 1:T_sim]
-y_tvlqr = [z_tvlqr[t][2] for t = 1:T_sim]
-plt = plot!(x_tvlqr,y_tvlqr,color=:purple,label="",width=2.0)
-
-z_sample, u_sample = simulate_linear_controller(K_sample,x_sol,u_sol,T_sim,Δt,z0_sim,w)
-x_sample = [z_sample[t][1] for t = 1:T_sim]
-y_sample = [z_sample[t][2] for t = 1:T_sim]
-plt = plot!(x_sample,y_sample,color=:orange,label="",width=2.0)
-
-# plot(hcat(u_tvlqr...)')
-# plot!(hcat(u_sample...)')
-#
-# K_sample
-# plot(vec(hcat(K_sample...) - hcat(K...)))
+plot(K_error,xlabel="time step",ylabel="norm(Ks-K)/norm(K)",yaxis=:log,width=2.0,label="β=$β",title="Gain matrix error")
